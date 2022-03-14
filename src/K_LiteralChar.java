14e68fd9a2d545d177ff32167dd334534e0ebf5a
PC 14 <rinaldy2402>
2022-03-14 19:30:07 +0700

    Percobaan mencetak literal

---
+++ src/K_LiteralChar.java
@@ -0,0 +1,13 @@
+public class K_LiteralChar {
+    public static void main(String[] args) {
+        char tab = '\t';
+        char neweline = '\n';
+        char singleQuete ='\'';
+        char doubleQuote ='\"';
+        System.out.println("Ini contoh" + tab + "literal tab");
+        System.out.println("Ini contoh " + neweline + "literal newline");
+        System.out.println("" + singleQuete + "Ini contoh literal single quote" + singleQuete);
+        System.out.println(""+ doubleQuote + "ini contoh literal double quote" + doubleQuote);
+
+    }
+}
