06b8f2cb7b518a31256de7c513694ae9a80b75a9
PC 14 <rinaldy2402>
2022-03-14 17:13:49 +0700

    Percobaan mencetak nilai kostanta

---
+++ src/G_Kostanta.java
@@ -0,0 +1,9 @@
+public class G_Kostanta {
+    public static void main(String[] args) {
+        final double PHI = 3.14;
+        final double CM_PER_INCH = 2.54;
+        System.out.println("Nilai Kostanta PHI = " + PHI);
+        System.out.println("Nilai kostanta CM_PER_INCH = " + CM_PER_INCH);
+
+    }
+}
