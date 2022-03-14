1ba7d96556d0a286896c598609c5cda61fdef965
PC 14 <rinaldy2402>
2022-03-14 15:11:57 +0700

    Inisial Project pertemuan 1 Dasar Java

---
+++ src/A_BlokdanKomentar.java
@@ -0,0 +1,14 @@
+public class A_BlokdanKomentar {
+    public static void main(String[] args) {
+        System.out.println("Dasar pemograman Java");
+        //Contoh kementar satu baris
+
+        /* contoh komentar lebih dari satu baris
+         */
+
+        /**
+         * Java Doc--> baris ini untuk mendokumentasikan kode program
+         */
+
+    }
+}
