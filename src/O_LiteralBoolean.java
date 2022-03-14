396b1318dd49172cf108340d43cf87e61295a28b
PC 14 <rinaldy2402>
2022-03-14 20:12:27 +0700

    Percobaan mencetak literal boolean

---
+++ src/O_LiteralBoolean.java
@@ -0,0 +1,8 @@
+public class O_LiteralBoolean {
+    public static void main(String args[]) {
+        boolean isOpen = true;
+        boolean isClose = false;
+        System.out.println("Contoh literal boolean kondisi" + isOpen);
+        System.out.println("Contoh literal boolean " + isClose);
+    }
+}
