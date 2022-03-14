007c558e8f848ff4a196913f807b1362660f6c31
PC 14 <rinaldy2402>
2022-03-14 20:20:08 +0700

    Percobaan ekpresi

---
+++ src/P_Ekpresi.java
@@ -0,0 +1,9 @@
+public class P_Ekpresi {
+    public static void main(String args[]) {
+        int bilA=5;
+        int bilB= bilA * 2;
+        int bilC= bilA + bilB;
+        System.out.println("Ini contoh ekpresi : bilA * 2 = " + bilB);
+        System.out.println("Ini contoh ekpresi : bilA + bilB" + bilC);
+    }
+}
