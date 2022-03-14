956d66a3c7940575133defceaf6012ef6c327cb7
PC 14 <rinaldy2402>
2022-03-14 20:24:17 +0700

    Percobaan statement

---
+++ src/Q_Statement.java
@@ -0,0 +1,10 @@
+public class Q_Statement {
+    public static void main(String[] args) {
+        System.out.print("Semoga saya bisa belarar");
+        System.out.println("Pemograman beriotasi Objek engan java");
+        int angka = 10;
+        char charA ='A';
+        System.out.println(angka);
+        System.out.println(charA);
+    }
+}
